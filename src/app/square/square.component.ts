5da5ef56dbe95c738e3bc3dc9404d67e36444b37
gagan231 <gagantripathi23@gmail.com>
2023-02-24 17:39:49 +0530

    Responsive deisgn fix.

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button nbButton *ngIf="!value" aria-label="btnPlay">{{ value }}</button>
-      <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-      <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+      <button nbButton *ngIf="!value" class="col" aria-label="btnPlay">{{ value }}</button>
+      <button nbButton hero status="success" aria-label="btnPlay" *ngIf="value == 'X'">{{ value }}</button>
+      <button nbButton hero status="info" aria-label="btnPlay" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
